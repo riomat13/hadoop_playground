739e8be4d18bfa69d326165f392741a46dfbdcf6
Rio <ryo.m1985@yahoo.com>
2020-02-18 19:40:36 -0800

    create python reducer object

--- src/python/main/base/mapreduce.py
+++ src/python/main/base/mapreduce.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
-import fileinput
 import logging
 
-"""Simple process streaming data with hadoop.
-
-Input data form:
-    Port Name,State,Port Code,Border,Date,Measure,Value
-"""
+import fileinput
+import operator
+import itertools
 
 log = logging.getLogger(__file__)
 
@@ -48,3 +45,35 @@ class Mapper(object):
                         yield row
             except Exception as e:
                 log.warning(f'Error occured in {repr(f)}: {e}')
+
+
+class Reducer(object):
+    def __init__(self, processor, sep=','):
+        """Base Reducer Class
+
+        This will read items from stdin processed by mapper
+
+        Args:
+            processor: function apply for map
+                processor function must return two items
+            sep: separator between key and value for sending data
+        """
+        self.func = processor
+        self.sep = sep
+
+    def reduce(self):
+        for key, group in itertools.groupby(self, operator.itemgetter(0)):
+            item = self.func(group)
+            self.emit(key, item)
+
+    def emit(self, key, value):
+        sys.stdout.write(f'{key}{self.sep}{value}\n')
+
+    def __iter__(self):
+        # read only from stdin
+        for content in sys.stdin:
+            try:
+                for row in content.splitlines():
+                    yield row.strip().split(self.sep)
+            except Exception as e:
+                log.warning(f'Error occured in {content}: {e}')
--- src/python/tests/base/test_mapreduce.py
+++ src/python/tests/base/test_mapreduce.py
@@ -6,7 +6,7 @@ import test.support
 
 import io
 
-from main.base.mapreduce import Mapper
+from main.base.mapreduce import Mapper, Reducer
 
 
 @patch('main.base.mapreduce.sys.stdout', new_callable=io.StringIO)
@@ -16,7 +16,7 @@ class MapperTest(unittest.TestCase):
         try:
             target = ['1,2', '3,4', '5,6']
 
-            self.mapper = Mapper(lambda x: x.split(','))
+            mapper = Mapper(lambda x: x.split(','))
             emitted = []
 
             with test.support.captured_stdin() as stdin:
@@ -25,7 +25,7 @@ class MapperTest(unittest.TestCase):
 
                 stdin.seek(0)
 
-                self.mapper.map()
+                mapper.map()
 
             # items output to stdout is stored in StringIO
             received = stdout.getvalue()
@@ -43,7 +43,7 @@ class MapperTest(unittest.TestCase):
             target = ['1,2', '3,4', '5,6']
             invalid = ['1,2,3', '4,5,6']
 
-            self.mapper = Mapper(lambda x: x.split(','))
+            mapper = Mapper(lambda x: x.split(','))
             emitted = []
 
             with test.support.captured_stdin() as stdin:
@@ -55,13 +55,60 @@ class MapperTest(unittest.TestCase):
                 # if invalid data is found, send log as warning
                 # and skip the data
                 with self.assertLogs(level='WARNING'):
-                    self.mapper.map()
+                    mapper.map()
 
             # items output to stdout is stored in StringIO
             received = stdout.getvalue()
             items = []
             for item in received.splitlines():
                 items.append(item)
+
+        finally:
+            stdout.close()
+
+
+@patch('main.base.mapreduce.sys.stdout', new_callable=io.StringIO)
+class ReducerTest(unittest.TestCase):
+
+    def test_read_output_from_mapper_and_reduce(self, stdout):
+        # simple suming task
+        def reduce_func(group):
+            total = 0
+            for item in group:
+                total += int(item[1])
+            return total
+
+        reducer = Reducer(reduce_func)
+
+        try:
+            # data must be sorted by key
+            contents = ['a,1', 'a,3', 'b,2', 'b,1']
+
+            # setup target data after reduced
+            target = {}
+            for item in contents:
+                k, v = item.split(',')
+                target[k] = target.get(k, 0) + int(v)
+
+            with test.support.captured_stdin() as stdin:
+                for line in contents:
+                    stdin.write(line + '\n')
+
+                stdin.seek(0)
+
+                reducer.reduce()
+
+            # items output to stdout is stored in StringIO
+            received = stdout.getvalue()
+            items = []
+            for item in received.splitlines():
+                items.append(item)
+
+            self.assertEqual(len(items), len(target))
+
+            for item in items:
+                k, v = item.strip().split(',')
+                self.assertEqual(int(v), target[k])
 
         finally:
             stdout.close()
